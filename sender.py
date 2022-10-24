155bc5fc3d5819d963d6e4f990c83b8e676b5b22
Naresh <20p31a0594@acet.ac.in>
2022-10-24 20:22:36 +0530

    updated files

--- sender.py
+++ sender.py
@@ -1,46 +1,36 @@
 import socket
-import random
 import time
+import random
 s = socket.socket()
 s.bind(("localhost",8020))
 s.listen(5)
-c, adr = s.accept()
-print(str(adr))
-n  = int(input("Enter number of frames: ")) 
+c, adr =s.accept()
+print("connection to " + str(adr) + " established")
+n = int(input("Enter number of frames: "))
 N = int(input("Enter window size: "))
-seq = 1 # is used to keep track of the window starting
-frame = 1  # frame to send starts with 1
-
-# send first N window size frames
+start = 0
+frame = 0
 for i in range(N):
-    print('Frames sent ->',frame)
+    print("sending --> ", frame)
     c.send(str(frame).encode())
-    frame += 1 
-    time.sleep(2)
+    frame += 1
+    time.sleep(1)
 
 timer = 5
-
-# will start with acknowledgement frame of 1
-while frame < n : 
-    t = random.randint(1,7)
+while start < n :
     msg = c.recv(1).decode()
     msg = int(msg)
-    print("Data received ", msg)
-    if(timer > t): 
-        # if the timer is greater than random number be consider it as ack
-        print("acknowledgement received")
-        print('Frames sent ->',str(frame))
-        # we will send next frame
-        c.send(str(frame).encode())
-        seq += 1
-        frame += 1 
+    t = random.randint(1, 7)
+    if(timer > t):
+        print("ack -->",start)
+        if(frame < n):
+            print("sending --> ",frame)
+            c.send(str(frame).encode())
+            frame += 1
+        start += 1
         time.sleep(2)
     else:
-        # if timer is less than the random number we consider as not received ack 
-        print('acknowledgement not received')
-        # we will again send the frames from window starting i.e seq
-        print('Frames sent ->',seq)
-        c.send(str(seq).encode())
-        time.sleep(2)
-    
-
+        print("Timeout")
+        print("sending again --> ",start)
+        c.send(str(start).encode())
+        time.sleep(1)
--- server1.py
+++ server1.py
@@ -5,43 +5,43 @@ s = socket.socket()
 s.bind(("localhost",8020))
 s.listen(5)
 c, adr =s.accept()
-print("address of the client ",str(adr))
+print("connection to " + str(adr) + " established")
 n = int(input("Enter number of frames: "))
 N = int(input("Enter window size: "))
-windowStart = 0
-windowEnd = 0
+start = 0
+end = 0
 frame = 0
 for i in range(N):
-    print("Frame sent --> ", frame)
+    print("sending --> ", frame)
     c.send(str(frame).encode())
     frame += 1
-    windowEnd += 1
+    end += 1
     time.sleep(1)
 
 timer = 5
 
-while windowStart < n :
+while start < n :
     msg = c.recv(1).decode()
     msg = int(msg)
 
-    if(msg != windowStart): 
+    if(msg != start): 
         continue
 
     t = random.randint(1, 7)
     if(timer > t):
-        print("acknowledgement received for ",windowStart)
+        print("ack --> ",start)
         if(frame < n):
-            print("frame sent -->",frame)
+            print("sending -->",frame)
             c.send(str(frame).encode())
             frame += 1
-            windowEnd += 1
-        windowStart += 1
+            end += 1
+        start += 1
         time.sleep(2)
     else:
-        print("acknowledgement not received for ",windowStart)
-        frame = windowStart
-        for i in range(windowStart, windowEnd):
-            print("Frame sent --> ",frame)
+        print("Timeout")
+        frame = start
+        for i in range(start, end):
+            print("sending again --> ",frame)
             c.send(str(frame).encode())
             frame += 1
             time.sleep(1)
